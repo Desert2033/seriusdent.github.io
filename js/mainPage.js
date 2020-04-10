d08e4b419d12734dd3adefccf03557c9cc281729
Администратор <tarnavskiy18@gmail.com>
2020-04-10 13:15:33 +0300

    ddd

---
+++ js/mainPage.js
@@ -0,0 +1,23 @@
+
+var wrapper = new Vue({
+    el: '#wrapper',
+    data: {
+        show: 'mainPage',
+        listPage: {
+            mainPage: 'mainPage',
+            aboutUs: 'aboutUs',
+            partner: 'partner',
+            services: 'services',
+            team: 'team',
+            contacts: 'contacts'
+        },
+    }
+});
+    
+//var aboutUs = new Vue({
+  //  el: '#aboutUs',
+    //data: {
+      //  show: false
+    //}
+//});
+
